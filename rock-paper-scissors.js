2ef87e14834ba0c20b11b200dd93f7a490fc02a5
Tamtox <39272747@protonmail.com>
2020-06-28 11:27:46 +0600

    Game finished

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
 //Computer's selection
-const arr = ["Rock","Paper","Scissors"]
 function computerChoice() {
+    const arr = ["Rock","Paper","Scissors"]
     let choice = Math.floor(Math.random()*arr.length);
     return arr[choice]
 }
@@ -9,19 +9,32 @@ let score = {
     Player:0,
     Computer:0,
 }
+//DOM
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+let  textResult = document.getElementById('text-result');
 //Game functionality
 function game(input) {
     let ai = computerChoice();
     console.log(ai)
     if((input ==="Rock" && ai==='Scissors') || (input === 'Paper' && ai === 'Rock') || (input === 'Scissors' && ai === 'Paper')) {
         score.Player+=1;
-        return `You Win!`
+        textResult.textContent="You win!"
+        playerScore.innerText = score.Player+""
     }
     else if ((input ==="Rock" && ai==='Paper') || (input === 'Paper' && ai === 'Scissors') || (input === 'Scissors' && ai === 'Rock')) {
         score.Computer+=1;
-        return `You Lose :(`
+        textResult.textContent="You lose!"
+        computerScore.innerText= score.Computer+""
     }
-    else if ((input ==="Rock" && ai==='Rock') || (input === 'Paper' && ai === 'Paper') || (input === 'Scissors' && ai === 'Scissors')) {
-        return `It's a draw, try again`
+    else {
+        textResult.textContent="It's a draw, try again."
     }
-}
+};
+//Events
+rock.addEventListener('click',function(){game("Rock")})
+paper.addEventListener('click',function(){game("Paper")})
+scissors.addEventListener('click',function(){game("Scissors")})
